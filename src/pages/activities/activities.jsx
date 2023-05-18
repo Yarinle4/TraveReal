1f0e8e08209cbee257802f40b1ee4604a3cdb7fb
DanaAdler <dana.adler1@mail.huji.ac.il>
2023-05-18 19:41:35 +0300

    people line and search lines

--- src/pages/activities/activities.jsx
+++ src/pages/activities/activities.jsx
@@ -1,8 +1,14 @@
 import "./activities.css";
 import ActivitiesCards from "../../components/ActivitiesJ";
 import ReactCardSlider from "../../components/ReactCardSlider";
-import Button from "@mui/material/Button";
 import ResponsiveAppBar from "../../shared/components/moreComponents/MainBar";
+import Avatar from "@mui/material/Avatar";
+import Box from '@mui/material/Box';
+import { useNavigate } from "react-router-dom";
+import Typography from '@mui/material/Typography';
+
+//people:
+import avatarPic from "../../assets/profile_picture_new.jpg";
 
 //events:
 import WineryTour from "../../assets/WineryTour.jpg";
@@ -21,6 +27,8 @@ import resturant from "../../assets/resturant.jpg";
 import SimpleBottomNavigation from "../../shared/components/moreComponents/BottomNav";
 
 function HomePageHost() {
+  const navigate = useNavigate();
+
   const sliderClick = (slider) => {
     alert("hello world");
   };
@@ -28,13 +36,47 @@ function HomePageHost() {
   const FirstsliderName = "addEvent";
   const SecondsliderName = "addTip";
 
+  const People = () => {
+    return (
+      <div
+      style={{
+        overflowY: 'auto', // Enable vertical scrolling
+        maxHeight: 'calc(100vh - 100px)', // Set the maximum height of the container
+      }}
+    >
+      <Box sx={{ display: 'flex', gap: '10px', ml:2, mr:2, mb:2 }}>
+        <div>
+        <Avatar sx={{ width: 80, height: 80, mb:1 }} alt="Person 1" src={avatarPic} onClick={() => navigate("/ProfilePage")} />
+        <Typography sx={{fontSize:'100%'}} align="center">Yarin Levy</Typography>
+        </div>
+        <div>
+        <Avatar sx={{ width: 80, height: 80, mb:1 }} alt="Person 2" src={avatarPic} onClick={() => navigate("/ProfilePage")} />
+        <Typography sx={{fontSize:'100%'}} align="center">Yarin Levy</Typography>
+        </div>
+        <div>
+        <Avatar sx={{ width: 80, height: 80, mb:1  }} alt="Person 3" src={avatarPic} onClick={() => navigate("/ProfilePage")} />
+        <Typography sx={{fontSize:'100%'}} align="center">Yarin Levy</Typography>
+        </div>
+        <div>
+        <Avatar sx={{ width: 80, height: 80, mb:1  }} alt="Person 4" src={avatarPic} onClick={() => navigate("/ProfilePage")} />
+        <Typography sx={{fontSize:'100%'}} align="center">Yarin Levy</Typography>
+        </div>
+        <div>
+        <Avatar sx={{ width: 80, height: 80, mb:1  }} alt="Person 5" src={avatarPic} onClick={() => navigate("/ProfilePage")} />
+        <Typography sx={{fontSize:'100%'}} align="center">Yarin Levy</Typography>
+        </div>
+        </Box>
+    </div>
+    );
+  };
+
   const events = [
     {
       image: WineryTour,
       title: "Winery Tour",
       description: "Cheers to good times",
       date: "Today | 5PM",
-      points: "10 stars.",
+      points: "10 stars",
       clickEvent: sliderClick,
     },
     {
@@ -42,7 +84,7 @@ function HomePageHost() {
       title: "Food Workshop",
       description: "Bring out the artist in you",
       date: "Tomorrow | 10AM",
-      points: "15 stars.",
+      points: "15 stars",
       clickEvent: sliderClick,
     },
     {
@@ -50,7 +92,7 @@ function HomePageHost() {
       title: "Street Food Tour",
       description: "Eat local, travel global",
       date: "Sun 30 Apr | 2PM",
-      points: "10 stars.",
+      points: "10 stars",
       clickEvent: sliderClick,
     },
     {
@@ -58,7 +100,7 @@ function HomePageHost() {
       title: "Food Tour",
       description: "Tasting the best",
       date: "Tue 2 May | 8PM",
-      points: "20 stars.",
+      points: "20 stars",
       clickEvent: sliderClick,
     },
     {
@@ -66,7 +108,7 @@ function HomePageHost() {
       title: "Boutique Dairies",
       description: "Say cheese!",
       date: "Thu 4 May | 10AM",
-      points: "5 stars.",
+      points: "5 stars",
       clickEvent: sliderClick,
     },
     {
@@ -74,7 +116,7 @@ function HomePageHost() {
       title: "Hafrashat Challah",
       description: "Observance of Torah mitzva",
       date: "Mon 8 May | 9PM",
-      points: "5 stars.",
+      points: "5 stars",
       clickEvent: sliderClick,
     },
   ];
@@ -94,7 +136,7 @@ function HomePageHost() {
     },
     {
       image: Bakery,
-      title: " Bakeries",
+      title: "Bakeries",
       description: "The best in town",
       clickEvent: sliderClick,
     },
@@ -112,24 +154,49 @@ function HomePageHost() {
     },
   ];
 
+ const Search = () => {
+    return(
+      <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <input placeholder='search' 
+      style={{
+        width: '90%',
+        borderRadius:7,
+        height: 30,
+        backgroundColor: 'white',
+        opacity:0.5,
+      }}>
+      </input>
+      </div>
+    )
+  }
+
   return (
     <div className="hostHome">
       <ResponsiveAppBar position="fixed" />
-
       <div id="upper">Welcome to the Culinary Circle!</div>
       <div class="body">
+        <div id="title">People</div>
+        <People/>
+      </div>
+      <div class="body">
         <div id="title">Upcoming Events</div>
+        <Search/>
         <div>
           <ActivitiesCards slides={events} idSlide={FirstsliderName} />
         </div>
-        {/* <Button variant="contained" color="primary" className="buttonHost">Another Event</Button> */}
       </div>
-      <div class="body">
+      <div class="buttomBody">
         <div id="title">Tips</div>
+        <Search/>
         <div>
           <ReactCardSlider slides={tips} idSlide={SecondsliderName} />
         </div>
-        {/* <Button variant="contained" color="primary" className="buttonHost">Another Tip</Button> */}
       </div>
       <SimpleBottomNavigation />
     </div>

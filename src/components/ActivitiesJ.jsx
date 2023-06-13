8650f82a07a29d59b8f6c0650e89ee6e82dfe45d
yarin4 <yarin.levy1@mail.huji.ac.il>
2023-06-13 18:39:31 +0300

    תמונה של איוונט

--- src/components/ActivitiesJ.jsx
+++ src/components/ActivitiesJ.jsx
@@ -1,70 +1,73 @@
-import React from 'react';
-import './ActivitiesCards.css';
-import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
-import AccessTimeRoundedIcon from '@mui/icons-material/AccessTimeRounded';
-import StarBorderRoundedIcon from '@mui/icons-material/StarBorderRounded';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+import React from "react";
+import "./ActivitiesCards.css";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import AccessTimeRoundedIcon from "@mui/icons-material/AccessTimeRounded";
+import StarBorderRoundedIcon from "@mui/icons-material/StarBorderRounded";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
+const CardSlider = (props) => {
+  const slideLeft = () => {
+    var slider = document.getElementById("slider");
+    var slider = document.getElementById(props.idSlide);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
 
-const CardSlider =(props)=>{
-    const slideLeft =()=>{
-        var slider = document.getElementById("slider");
-        var slider = document.getElementById(props.idSlide);
-        slider.scrollLeft = slider.scrollLeft - 500;
-    }
+  const slideRight = () => {
+    var slider = document.getElementById("slider");
+    var slider = document.getElementById(props.idSlide);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
 
-    const slideRight =()=>{
-        var slider = document.getElementById("slider");
-        var slider = document.getElementById(props.idSlide);
-        slider.scrollLeft = slider.scrollLeft + 500;
-    }
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-
-    return (
-        <div id="main-slider-container">
-            <MdChevronLeft 
-                size={40} 
-                className="slider-icon left" 
-                onClick={slideLeft}
-            />
-            <div class="slider" id={props.idPeople}>
-               {props.slides.map((slide, index) => {
-                        return (
-                            // <div className="slider-card" key={index} onClick={()=>slide.clickEvent()}> להשתמש בזה אחכ
-                            <div 
-                                className="slider-card" 
-                                key={index} 
-                                onClick={() => navigate("/EventPage")}
-                            >
-                                <div 
-                                    className="slider-card-image" 
-                                    style={{
-                                        backgroundImage:`url(${slide.image})`,
-                                        backgroundSize:'cover',
-                                    }}
-                                ></div>
-                                <p className="slider-card-title">{slide.name}</p>
-                                {/* <p className="slider-card-description">{slide.description}</p> */}
-                                <hr class="slant-line"></hr>
-                                <p className="slider-card-date">
-                                    <AccessTimeRoundedIcon fontSize="small" sx={{ml:1, mr:1}}/>{slide.date + " | " + slide.time}</p>
-                                {/* <p className="slider-card-points">
+  return (
+    <div id="main-slider-container">
+      <MdChevronLeft
+        size={40}
+        className="slider-icon left"
+        onClick={slideLeft}
+      />
+      <div class="slider" id={props.idPeople}>
+        {props.slides.map((slide, index) => {
+          return (
+            // <div className="slider-card" key={index} onClick={()=>slide.clickEvent()}> להשתמש בזה אחכ
+            <div
+              className="slider-card"
+              key={index}
+              onClick={() => navigate("/EventPage")}
+            >
+              <div
+                className="slider-card-image"
+                style={{
+                  backgroundImage: `url(${slide.eventPictureUrl})`,
+                  backgroundSize: "cover",
+                }}
+              ></div>
+              <p className="slider-card-title">{slide.name}</p>
+              {/* <p className="slider-card-description">{slide.description}</p> */}
+              <hr class="slant-line"></hr>
+              <p className="slider-card-date">
+                <AccessTimeRoundedIcon fontSize="small" sx={{ ml: 1, mr: 1 }} />
+                {slide.date + " | " + slide.time}
+              </p>
+              {/* <p className="slider-card-points">
                                     <StarBorderRoundedIcon fontSize="medium" sx={{ml:0.9, mr:1}}/>{slide.points}</p> */}
-                                <p className="slider-card-location">
-                                    <LocationOnIcon fontSize="medium" sx={{ml:0.9, mr:1}}/>{slide.location}</p>
-                            </div>
-                        );
-                })}
+              <p className="slider-card-location">
+                <LocationOnIcon fontSize="medium" sx={{ ml: 0.9, mr: 1 }} />
+                {slide.location}
+              </p>
             </div>
-            <MdChevronRight 
-                size={40} 
-                className="slider-icon right" 
-                onClick={slideRight}
-            />
-        </div>
-    );
+          );
+        })}
+      </div>
+      <MdChevronRight
+        size={40}
+        className="slider-icon right"
+        onClick={slideRight}
+      />
+    </div>
+  );
 };
 export default CardSlider;
